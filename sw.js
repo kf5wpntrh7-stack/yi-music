c17b249b08db0a912b7f0b8cecaa33a5927644b9
kf5wpntrh7-stack <kf5wpntrh7@privaterelay.appleid.com>
2026-09-23 23:09:30 +0800

    Improve yi Music: sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yi-music-shell-v4';
+const CACHE = 'yi-music-shell-v5';
 const SHELL = ['./', './index.html', './styles.css', './app.js', './playlist.js', './manifest.webmanifest', './favicon.svg'];
 
 self.addEventListener('install', (event) => {
