f05df95d5b7b4a593c099476c75d6595e727c9b5
kf5wpntrh7-stack <kf5wpntrh7@privaterelay.appleid.com>
2026-09-24 09:39:17 +0800

    Keep black-screen control available in compact player

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yi-music-shell-v7';
+const CACHE = 'yi-music-shell-v8';
 const SHELL = ['./', './index.html', './styles.css', './app.js', './playlist.js', './manifest.webmanifest', './favicon.svg'];
 
 self.addEventListener('install', (event) => {
