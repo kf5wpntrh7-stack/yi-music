23a7f8904adb23b6749ea55b37bf8bdbfca4fb20
kf5wpntrh7-stack <kf5wpntrh7@privaterelay.appleid.com>
2026-09-24 16:49:56 +0800

    Refresh song numbering in offline cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yi-music-shell-v11';
+const CACHE = 'yi-music-shell-v12';
 const SHELL = ['./', './index.html', './styles.css', './app.js', './playlist.js', './manifest.webmanifest', './favicon.svg'];
 
 self.addEventListener('install', (event) => {
